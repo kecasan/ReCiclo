5245019a881d61963d9fc54a5a315e9db84a93d3
Érica <kecasantos323@gmail.com>
2025-11-02 22:00:26 -0300

    Cria view, template e URL para listar notícias

--- noticias/views.py
+++ noticias/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Noticia
 
-# Create your views here.
+def lista_noticias(request):
+    #1. Buscar todos os objetos 'Noticia' no banco de dados
+    todas_as_noticias = Noticia.objects.all().order_by('-data_publicacao')
+    
+    #2. Definir o 'contexto'que será enviado para o template
+    contexto ={
+        'noticias': todas_as_noticias,
+    }
+
+    #3. Renderizar o template HTML com os dados do contexto
+    return render(request, 'noticias/lista_noticias.html', contexto)
