89f066d29aeda790c6ed585c7cbfa6f4522c89c4
Érica <kecasantos323@gmail.com>
2025-11-03 01:07:09 -0300

    Futura conexão com banco de dados em Produção

--- reciclo/settings.py
+++ reciclo/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,6 +67,9 @@ WSGI_APPLICATION = 'reciclo.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    #DATABASES = {
+    #'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    #}
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
