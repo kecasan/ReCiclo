430b382fb5c14c1cca57ce0f56f47a81cc6e6b6c
Érica <kecasantos323@gmail.com>
2025-11-02 22:27:34 -0300

    Cria view, template e URL para página de detalhe de notícia

--- noticias/urls.py
+++ noticias/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+# Damos um 'nome' ao nosso conjunto de urls para organizar melhor
+app_name = 'noticias'
+
 urlpatterns = [
+    # ex: /noticias/
     path('', views.lista_noticias, name='lista_noticias'),
+    
+    # ex: /noticias/5/
+    path('<int:pk>/', views.detalhe_noticia, name='detalhe_noticia'),
 ]
--- noticias/views.py
+++ noticias/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 # Adicione get_object_or_404
 from .models import Noticia
 
 def lista_noticias(request):
-    #1. Buscar todos os objetos 'Noticia' no banco de dados
     todas_as_noticias = Noticia.objects.all().order_by('-data_publicacao')
-    
-    #2. Definir o 'contexto'que será enviado para o template
-    contexto ={
+    contexto = {
         'noticias': todas_as_noticias,
     }
-
-    #3. Renderizar o template HTML com os dados do contexto
     return render(request, 'noticias/lista_noticias.html', contexto)
+
+# --- NOSSA NOVA VIEW ---
+def detalhe_noticia(request, pk):
+    # Busca a notícia com a 'pk' (ID) vinda da URL, ou mostra um erro 404 se não encontrar
+    noticia = get_object_or_404(Noticia, pk=pk)
+    
+    contexto = {
+        'noticia': noticia, # Passamos apenas a notícia encontrada
+    }
+    return render(request, 'noticias/detalhe_noticia.html', contexto)
